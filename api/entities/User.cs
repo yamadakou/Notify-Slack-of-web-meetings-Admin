6290569c30022580968d3d44608ade2f41624385
minoura-a <ayaka.minoura.j4p@jpgr.denso.com>
2022-07-25 14:58:42 +0900

    Userクラスを追加

---
+++ api/entities/User.cs
@@ -0,0 +1,43 @@
+using System;
+using Newtonsoft.Json;
+
+namespace NotifySlackOfWebMeetingsAdmin.Apis.entities
+{
+    /// <summary>
+    /// ユーザーを表す
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// 既定のコンストラクタ
+        /// </summary>
+        public User()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        /// <summary>
+        /// 一意とするID
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// ユーザー名
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Eメールアドレス
+        /// </summary>
+        [JsonProperty("emailAddress")]
+        public string EmailAddress { get; set; }
+        
+        /// <summary>
+        /// ユーザープリンシパル名
+        /// </summary>
+        [JsonProperty("userPrincipal")]
+        public string UserPrincipal { get; set; }
+    }
+}
