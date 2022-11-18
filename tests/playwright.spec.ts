b33c3a435b01a1d93ac8e546ebbf18f1bd0a7b10
minoura-a <ayaka.minoura.j4p@jpgr.denso.com>
2022-11-18 11:30:40 +0900

    Revert "テストをスキップするよう実装"
    This reverts commit 8f9408b3f12af464dbc90b50b6a1171b7e481aa4.

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.skip('basic test', async ({ page }) => {
+test('basic test', async ({ page }) => {
   await page.goto('/');
   await expect(page.locator('div div')).toContainText('Hello World');
 })
