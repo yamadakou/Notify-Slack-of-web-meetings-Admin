f5a1d80ae7f51033d48a6eb3a15afdf11bb403c1
koji.yamada,j7k <koji.yamada,j7k@jpgr.denso.com>
2023-04-19 16:30:58 +0900

    R 認可トークンの項目追加

--- api/entities/User.cs
+++ api/entities/User.cs
@@ -1,37 +1,49 @@
 using System;
 using Newtonsoft.Json;
 
-namespace NotifySlackOfWebMeetingsAdmin.Apis.entities
+namespace NotifySlackWebMeetingsAdmin.Api.Entities
 {
+  /// <summary>
+  /// Slackチャンネルを表す
+  /// </summary>
+  public class User
+  {
     /// <summary>
-    /// ユーザーを表す
+    /// 既定のコンストラクタ
     /// </summary>
-    public class User
+    public User()
     {
-        /// <summary>
-        /// 既定のコンストラクタ
-        /// </summary>
-        public User()
-        {
-            Id = Guid.NewGuid().ToString();
-        }
-
-        /// <summary>
-        /// 一意とするID
-        /// </summary>
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        /// <summary>
-        /// ユーザー名
-        /// </summary>
-        [JsonProperty("name")]
-        public string Name { get; set; }
-        
-        /// <summary>
-        /// Eメールアドレス
-        /// </summary>
-        [JsonProperty("emailAddress")]
-        public string EmailAddress { get; set; }
+      Id = Guid.NewGuid().ToString();
+      AuthorizationKey = Guid.NewGuid().ToString();
     }
+
+    #region プロパティ
+    /// <summary>
+    /// 一意とするID
+    /// </summary>
+    [JsonProperty("id")]
+    public string Id { get; set; }
+    /// <summary>
+    /// ユーザー名
+    /// </summary>
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    /// <summary>
+    /// Eメールアドレス
+    /// </summary>   
+    [JsonProperty("emailAddress")]
+    public string EmailAddress { get; set; }
+    /// <summary>
+    /// 登録日時（UTC）
+    /// </summary>
+    [JsonProperty("registeredAt")]
+    public DateTime RegisteredAt { get; set; }
+    /// <summary>
+    /// 認可トークン
+    /// </summary>   
+    [JsonProperty("authorizationKey")]
+    public string AuthorizationKey { get; set; }
+
+    #endregion
+  }
 }
